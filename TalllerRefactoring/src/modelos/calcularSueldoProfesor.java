13e22e950ac6f3e8133156393cb0c4d8e57caa95
Jeremy-Ramirez <66746920+Jeremy-Ramirez@users.noreply.github.com>
2020-08-13 21:01:00 -0500

    Solución Temporary fields

--- TalllerRefactoring/src/modelos/calcularSueldoProfesor.java
+++ TalllerRefactoring/src/modelos/calcularSueldoProfesor.java
@@ -3,8 +3,6 @@ package modelos;
 public class calcularSueldoProfesor {
     
     public double calcularSueldo(Profesor prof){
-        double sueldo=0;
-        sueldo= prof.añosdeTrabajo*600 + prof.BonoFijo;
-        return sueldo;
+        return prof.añosdeTrabajo*600 + prof.BonoFijo;
     }  
 }
